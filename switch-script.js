7f69403939869121167aa170109d550f4e0e259f
loyalfriend1109 <danielwalker021109@gmail.com>
2025-01-24 20:43:01 -0500

    tel call added, SEO added, extra Greek version added

--- switch-script.js
+++ switch-script.js
@@ -7,4 +7,8 @@ function switchlanguage() {
     elements.forEach(element => {
       element.classList.toggle('hidden');
     });
+    document.getElementById('name').placeholder = document.getElementById('name').placeholder == "Name" ? "Όνομα" : "Name";
+    document.getElementById('email').placeholder = document.getElementById('email').placeholder == "Email" ? "Ηλεκτρονική Διεύθυνση" : "Email";
+    document.getElementById('message').placeholder = document.getElementById('message').placeholder == "Tell us about your project" ? "Πείτε μας για το έργο σας" : "Tell us about your project";
   }
+
